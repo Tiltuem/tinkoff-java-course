521562f3c412019122fe2a4a73e5961ca8b4eb58
Tiltuem <98845422+Tiltuem@users.noreply.github.com>
2023-11-18 02:17:02 +0400

    feat: remove freeTest and occupiedTest

--- src/test/java/edu/hw6/Task6Test.java
+++ src/test/java/edu/hw6/Task6Test.java
@@ -1,9 +1,6 @@
 package edu.hw6;
 
-import java.io.IOException;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.ServerSocket;
+
 import java.util.HashSet;
 import java.util.Set;
 import org.junit.jupiter.api.DisplayName;
@@ -27,28 +24,5 @@ public class Task6Test {
         assertThat(udpSet.size()).isEqualTo(2);
     }
 
-    @Test
-    @DisplayName("checkOccupiedPort")
-    public void checkOccupiedPort() {
-        Task6 portChecker = new Task6();
-
-        try (ServerSocket serverSocket = new ServerSocket(80);
-             DatagramSocket datagramSocket = new DatagramSocket(80, InetAddress.getByName("localhost"))
-        ) {
-            assertThat(portChecker.checkPort(80, "TCP")).isFalse();
-            assertThat(portChecker.checkPort(80, "UDP")).isFalse();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Test
-    @DisplayName("checkFreePort")
-    public void checkFreePort() {
-        Task6 portChecker = new Task6();
-
-        assertThat(portChecker.checkPort(800, "UDP")).isTrue();
-        assertThat(portChecker.checkPort(800, "TCP")).isTrue();
-    }
 }
 
