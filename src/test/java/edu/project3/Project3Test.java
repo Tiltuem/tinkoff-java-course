3f12e77d9d9d3668e343d32b109d3fb16830552e
Tiltuem <98845422+Tiltuem@users.noreply.github.com>
2023-11-22 15:24:46 +0400

    feat: remove writeAdoc

--- src/test/java/edu/project3/Project3Test.java
+++ src/test/java/edu/project3/Project3Test.java
@@ -27,21 +27,13 @@ public class Project3Test {
         File fileMdValid = new File("log.md");
         writeMd(fileMdValid);
 
-        File fileAdocValid = new File("log.adoc");
-        writeAdoc(fileAdocValid);
-
         try {
             byte[] md = Files.readAllBytes(fileMd.toPath());
             byte[] validMd = Files.readAllBytes(fileMdValid.toPath());
 
-            byte[] adoc = Files.readAllBytes(fileAdoc.toPath());
-            byte[] validAdoc = Files.readAllBytes(fileAdocValid.toPath());
-
             assertThat(md).isEqualTo(validMd);
-            assertThat(adoc).isEqualTo(validAdoc);
 
             Files.deleteIfExists(fileMdValid.toPath());
-            Files.deleteIfExists(fileAdocValid.toPath());
             Files.deleteIfExists(fileMd.toPath());
             Files.deleteIfExists(fileAdoc.toPath());
         } catch (IOException e) {
@@ -115,85 +107,6 @@ public class Project3Test {
                 | Debian APT-HTTP/1.3 (0.8.16~exp12ubuntu10.16) | 5740 |
                 | Debian APT-HTTP/1.3 (0.8.16~exp12ubuntu10.22) | 3855 |
                     """);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void writeAdoc(File fileAdocValid) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileAdocValid))) {
-            writer.write("""
-                == Общая информация
-
-                |===
-                |Метрика |Значение
-                |Файл(ы) |src\\main\\resources\\logs\\logs.txt, src\\main\\resources\\logs\\test.txt
-                |Начальная дата |null
-                |Конечная дата |null
-                |Кол-во запросов |70661
-                |Средний размер ответа |480762 b
-                |===
-                == Коды ответа
-
-                |===
-                |Код |Значение |Количество
-                |404 |Not Found |34533\s
-                |200 |OK |20035\s
-                |304 |Not Modified |13330\s
-                |400 |Bad Request |649\s
-                |301 |UNKNOWN |647\s
-                |500 |Internal Server Error |624\s
-                |302 |UNKNOWN |615\s
-                |206 |Partial Content |186\s
-                |403 |Forbidden |38\s
-                |416 |Range Not Satisfiable |4\s
-                |===
-                == Запрашиваемые ресурсы
-
-                [cols=\\"^,^\\",options=\\"header\\",]
-                |===
-                |РЕСУРС |Количество
-                |/product_1 |30285
-                |/product_2 |21104
-                |/product_3 |73
-                |/7.htm |13
-                |/Reverse-engineered.css |12
-                |===
-                == Количество запросов с IP-адреса
-
-                [cols=\\"^,^\\",options=\\"header\\",]
-                |===
-                |IP |Количество
-                |216.46.173.126 |2350
-                |180.179.174.219 |1720
-                |204.77.168.241 |1439
-                |65.39.197.164 |1365
-                |80.91.33.133 |1202
-                |===
-                == HTTP-запросы
-
-                [cols=\\"^,^\\",options=\\"header\\",]
-                |===
-                |МЕТОД |Количество
-                |GET |64154
-                |HEAD |1394
-                |POST |1322
-                |PUT |1269
-                |DELETE |1262
-                |PATCH |1260
-                |===
-                == User-Agent
-
-                [cols=\\"^,^\\",options=\\"header\\",]
-                |===
-                |АГЕНТ |Количество
-                |Debian APT-HTTP/1.3 (1.0.1ubuntu2) |11830
-                |Debian APT-HTTP/1.3 (0.9.7.9) |11365
-                |Debian APT-HTTP/1.3 (0.8.16~exp12ubuntu10.21) |6719
-                |Debian APT-HTTP/1.3 (0.8.16~exp12ubuntu10.16) |5740
-                |Debian APT-HTTP/1.3 (0.8.16~exp12ubuntu10.22) |3855
-                |===
-                """);
         } catch (IOException e) {
             e.printStackTrace();
         }
